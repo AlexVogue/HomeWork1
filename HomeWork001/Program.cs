c293a4bf625e71ee06a404049640db192c5fd725
AlexVogue <soloviev.a@bk.ru>
2022-06-27 18:06:37 +0300

    Найти наибольшее и наименьшее из 2-х чисел

---
+++ HomeWork001/Program.cs
@@ -0,0 +1,26 @@
+﻿   // Найти наибольшее и наименьшее из 2-х чисел.
+   
+   int a, b;
+ 
+        Console.WriteLine("Введите два числа : ");
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+ 
+        if (a > b)
+ 
+        {
+ 
+            Console.WriteLine("{0} Наибольшее число", a);
+ 
+        }
+ 
+        else
+        {
+ 
+            Console.WriteLine("{0} Наибольшее число ", b);
+ 
+        }
+ 
+        Console.ReadLine();
